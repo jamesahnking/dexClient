699390c94b886153ff262b245cbf2ca7fcd1910b
jamesahnking <jamesahnking@users.noreply.github.com>
2022-03-04 08:47:46 -0500

    - Added sass dependency - Added scss directory and files for the exchange - Added Bootstrap min to index.js - Renamed taderList.scss to tradeList.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import './scss/index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LoadingContainer from './LoadingContainer';
 import reportWebVitals from './reportWebVitals';
 
